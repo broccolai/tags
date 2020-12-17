a3975d8bc4f1c71474bbbdeb8323b55b1eeef179
broccolai <me@broccol.ai>
2020-12-17 02:11:27 +0000

    More work on tag command

--- src/main/java/broccolai/tags/commands/TagsCommand.java
+++ src/main/java/broccolai/tags/commands/TagsCommand.java
@@ -1,6 +1,8 @@
 package broccolai.tags.commands;
 
 import broccolai.tags.commands.context.CommandUser;
+import broccolai.tags.factory.CloudArgumentFactory;
+import broccolai.tags.model.tag.Tag;
 import broccolai.tags.model.user.TagsUser;
 import broccolai.tags.service.user.UserPipeline;
 import cloud.commandframework.Command;
@@ -17,19 +19,26 @@ public final class TagsCommand {
     private final @NonNull UserPipeline userPipeline;
 
     @Inject
-    public TagsCommand(final @NonNull CommandManager<CommandUser> manager, final @NonNull UserPipeline userPipeline) {
+    public TagsCommand(
+            final @NonNull CommandManager<CommandUser> manager,
+            final @NonNull CloudArgumentFactory argumentFactory,
+            final @NonNull UserPipeline userPipeline
+    ) {
         this.userPipeline = userPipeline;
 
         Command.Builder<CommandUser> tagsCommand = manager.commandBuilder("tags");
 
-        manager.command(tagsCommand.handler(this::handleTags));
+        manager.command(tagsCommand
+                .argument(argumentFactory.tag("tag"))
+                .handler(this::handleTags));
     }
 
     private void handleTags(final @NonNull CommandContext<CommandUser> context) {
         CommandUser sender = context.getSender();
         TagsUser user = this.userPipeline.get(new UUID(0, 0));
+        Tag tag = context.get("tag");
 
-        sender.sendMessage(Component.text(user.getClass().getSimpleName()));
+        sender.sendMessage(Component.text(tag.name()));
     }
 
 }
