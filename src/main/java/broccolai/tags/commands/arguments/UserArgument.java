6628c4c14644d78006b7e3e9a664929720e0ceb9
broccolai <me@broccol.ai>
2020-12-20 17:46:56 +0000

    Add user argument and bind it to factory

---
+++ src/main/java/broccolai/tags/commands/arguments/UserArgument.java
@@ -0,0 +1,76 @@
+package broccolai.tags.commands.arguments;
+
+import broccolai.tags.commands.context.CommandUser;
+import broccolai.tags.model.user.TagsUser;
+import broccolai.tags.service.user.UserPipeline;
+import cloud.commandframework.arguments.CommandArgument;
+import cloud.commandframework.arguments.parser.ArgumentParseResult;
+import cloud.commandframework.arguments.parser.ArgumentParser;
+import cloud.commandframework.context.CommandContext;
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class UserArgument extends CommandArgument<@NonNull CommandUser, @NonNull TagsUser> {
+
+    @AssistedInject
+    public UserArgument(final @NonNull UserPipeline userPipeline, final @Assisted("name") @NonNull String name) {
+        super(true, name, new UserParser(userPipeline), TagsUser.class);
+    }
+
+    public static final class UserParser implements ArgumentParser<@NonNull CommandUser, TagsUser> {
+
+        private final @NonNull UserPipeline userPipeline;
+
+        public UserParser(final @NonNull UserPipeline userPipeline) {
+            this.userPipeline = userPipeline;
+        }
+
+        @Override
+        public @NonNull ArgumentParseResult<TagsUser> parse(
+                @NonNull final CommandContext<@NonNull CommandUser> commandContext,
+                @NonNull final Queue<String> inputQueue
+        ) {
+            final String input = inputQueue.peek();
+
+            if (input == null) {
+                return ArgumentParseResult.failure(new NullPointerException("Expected tag name"));
+            }
+
+            //todo: UUID Pipeline?
+            //noinspection deprecation
+            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(input);
+            TagsUser user = this.userPipeline.get(offlinePlayer.getUniqueId());
+
+            if (user == null) {
+                return ArgumentParseResult.failure(new NullPointerException("Could not find tag with name " + input));
+            }
+
+            inputQueue.remove();
+            return ArgumentParseResult.success(user);
+        }
+
+        @Override
+        public @NonNull List<String> suggestions(
+                @NonNull final CommandContext<@NonNull CommandUser> commandContext,
+                @NonNull final String input
+        ) {
+            List<String> output = new ArrayList<>();
+
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                output.add(player.getName());
+            }
+
+            return output;
+        }
+
+    }
+
+}
--- src/main/java/broccolai/tags/factory/CloudArgumentFactory.java
+++ src/main/java/broccolai/tags/factory/CloudArgumentFactory.java
@@ -1,11 +1,14 @@
 package broccolai.tags.factory;
 
 import broccolai.tags.commands.arguments.TagArgument;
+import broccolai.tags.commands.arguments.UserArgument;
 import com.google.inject.assistedinject.Assisted;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 public interface CloudArgumentFactory {
 
     @NonNull TagArgument tag(@Assisted("name") @NonNull String name);
+
+    @NonNull UserArgument user(@Assisted("name") @NonNull String name);
 
 }
