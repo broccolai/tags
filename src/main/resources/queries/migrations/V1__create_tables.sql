7d943a0b9464e24bf7fd4ed8c9b5e92d2231a4cf
broccolai <me@broccol.ai>
2020-12-21 01:15:12 +0000

    Remove tags_tags table

--- src/main/resources/queries/migrations/V1__create_tables.sql
+++ src/main/resources/queries/migrations/V1__create_tables.sql
@@ -1,14 +1,7 @@
-CREATE TABLE tags_tags
-(
-    id    INTEGER,
-    owner CHAR(36),
-
-    PRIMARY KEY (id, owner)
-);
-
 CREATE TABLE tags_users
 (
     uuid    CHAR(36),
+    currentTag INTEGER,
 
     PRIMARY KEY (uuid)
 );
