55a511b94f67851e5494452ad445e3cfd61e01ec
broccolai <me@broccol.ai>
2020-12-22 03:25:09 +0000

    All allTags method to TagsService

--- src/main/java/broccolai/tags/service/tags/TagsService.java
+++ src/main/java/broccolai/tags/service/tags/TagsService.java
@@ -3,15 +3,19 @@ package broccolai.tags.service.tags;
 import broccolai.tags.config.Configuration;
 import broccolai.tags.config.TagConfiguration;
 import broccolai.tags.model.tag.Tag;
+import broccolai.tags.model.user.TagsUser;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.milkbowl.vault.permission.Permission;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Singleton
@@ -19,11 +23,15 @@ public final class TagsService {
 
     private static final MiniMessage MINI = MiniMessage.get();
 
+    private final @NonNull Permission permission;
+
     private final @NonNull Map<Integer, Tag> idToTags = new HashMap<>();
     private final @NonNull Map<String, Tag> nameToTags = new HashMap<>();
 
     @Inject
-    public TagsService(final @NonNull Configuration configuration) {
+    public TagsService(final @NonNull Configuration configuration, final @NonNull Permission permission) {
+        this.permission = permission;
+
         for (TagConfiguration config : configuration.tags) {
             this.create(config.id, config.name, config.secret, config.component, config.reason);
         }
@@ -54,6 +62,13 @@ public final class TagsService {
 
     public Collection<Tag> allTags() {
         return Collections.unmodifiableCollection(this.idToTags.values());
+    }
+
+    public Collection<Tag> allTags(final @NonNull TagsUser user) {
+        List<Tag> filtered = new ArrayList<>(this.idToTags.values());
+        filtered.removeIf(tag -> !user.hasPermission(permission, "tags.tag." + tag.id()));
+
+        return Collections.unmodifiableCollection(filtered);
     }
 
 }
