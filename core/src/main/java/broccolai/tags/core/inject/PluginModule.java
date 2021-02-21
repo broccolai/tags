606fba0e9e090e3006a5a4097b8ea2d25787da04
broccolai <me@broccol.ai>
2021-02-21 07:23:34 +0000

    Delete database module in favour of plugin module

---
+++ core/src/main/java/broccolai/tags/core/inject/PluginModule.java
@@ -0,0 +1,46 @@
+package broccolai.tags.core.inject;
+
+import broccolai.tags.api.model.user.TagsUser;
+import broccolai.tags.core.config.MainConfiguration;
+import broccolai.tags.core.data.StorageType;
+import broccolai.tags.core.data.jdbi.UserMapper;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.io.File;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jdbi.v3.core.Jdbi;
+
+public final class PluginModule extends AbstractModule {
+
+    @Provides
+    @Singleton
+    public @NonNull HikariDataSource provideDataSource(
+            final @NonNull File folder,
+            final @NonNull MainConfiguration configuration
+    ) {
+        HikariConfig hikariConfig = new HikariConfig();
+
+        if (configuration.storage.storageType == StorageType.SQLITE) {
+            File file = new File(folder, "tags.db");
+            hikariConfig.setJdbcUrl("jdbc:sqlite:" + file);
+        }
+
+        hikariConfig.setMaximumPoolSize(10);
+        return new HikariDataSource(hikariConfig);
+    }
+
+    @Provides
+    @Singleton
+    public @NonNull Jdbi provideJdbi(
+            final @NonNull HikariDataSource dataSource
+    ) {
+        return Jdbi.create(dataSource)
+                .registerRowMapper(TagsUser.class, new UserMapper());
+    }
+
+}
